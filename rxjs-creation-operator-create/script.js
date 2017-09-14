ddfa952ce6273528e571398f2427c4c52ca51a60
Zac Jones <zacjones93@gmail.com>
2017-09-14 16:04:45 -0700

    prepare rxjs-creation-operator-create plunk

--- rxjs-creation-operator-create/script.js
+++ rxjs-creation-operator-create/script.js
@@ -8,7 +8,16 @@ function subscribe(observer) {
 var foo = new Rx.Observable(subscribe);
 
 foo.subscribe(
-  function (x) { console.log('next ' + x); },
-  function (err) { console.log('error ' + err); },
-  function () { console.log('done'); },
+  // displayInPreview adds the log statement to the Plunker Preview
+  function (x) { console.log('next ' + x) || displayInPreview('next ' + x); },
+  function (err) { console.log('error ' + err) || displayInPreview('error ' + err); },
+  function () { console.log('done') || displayInPreview('done'); },
 );
+
+// display in plunker preview
+function displayInPreview(string) {
+  var newDiv = document.createElement("div"); 
+  var newContent = document.createTextNode(string); 
+  newDiv.appendChild(newContent);
+  document.body.appendChild(newDiv)
+}
