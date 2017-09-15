746ffb4f2255debb56ad2098ffa9010432bd63fc
lsminter <lsminteris@gmail.com>
2017-09-15 14:53:49 -0700

    prepare plunk

--- rxjs-creation-operators-empty-never-throw/script.js
+++
@@ -1,9 +0,0 @@
-var foo = Rx.Observable.empty();
-
-foo.subscribe(function (x) {
-  console.log('next ' + x);
-}, function (err) {
-  console.log('error ' + err);
-}, function () {
-  console.log('done');
-});
